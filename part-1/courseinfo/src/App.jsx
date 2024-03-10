c845eb19bede49598c7d77de042387ebf9b5e097
Alex Guidace <alexguidace@gmail.com>
2024-03-09 21:02:57 -0800

    For Course Information App, move part objects into an array and adjust associated code

--- part-1/courseinfo/src/App.jsx
+++ part-1/courseinfo/src/App.jsx
@@ -5,17 +5,17 @@ const Header = ({ course }) => {
 const Content = ({ content }) => {
   return (
     <>
-      <Part partExerciseData={content[0]} />
-      <Part partExerciseData={content[1]} />
-      <Part partExerciseData={content[2]} />
+      <Part partDataObject={content[0]} />
+      <Part partDataObject={content[1]} />
+      <Part partDataObject={content[2]} />
     </>
   );
 };
 
-const Part = ({ partExerciseData }) => {
+const Part = ({ partDataObject }) => {
   return (
     <p>
-      {partExerciseData.name} {partExerciseData.exercises}
+      {partDataObject.name} {partDataObject.exercises}
     </p>
   );
 };
@@ -26,24 +26,28 @@ const Total = ({ totalExercises }) => {
 
 const App = () => {
   const course = 'Half Stack application development';
-  const part1 = {
-    name: 'Fundamentals of React',
-    exercises: 10,
-  };
-  const part2 = {
-    name: 'Using props to pass data',
-    exercises: 7,
-  };
-  const part3 = {
-    name: 'State of a component',
-    exercises: 14,
-  };
-  const totalExercises = part1.exercises + part2.exercises + part3.exercises;
+  const parts = [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10,
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7,
+    },
+    {
+      name: 'State of a component',
+      exercises: 14,
+    },
+  ];
+
+  const totalExercises =
+    parts[0].exercises + parts[1].exercises + parts[2].exercises;
 
   return (
     <div>
       <Header course={course} />
-      <Content content={[part1, part2, part3]} />
+      <Content content={parts} />
       <Total totalExercises={totalExercises} />
     </div>
   );
