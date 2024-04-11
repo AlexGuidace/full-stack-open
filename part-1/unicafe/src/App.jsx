bd8af0b17c8155fb277aaf06b9b1cfa1f428be3d
Alex Guidace <alexguidace@gmail.com>
2024-04-10 18:31:14 -0700

    Complete UniCafe Exercise 1.7 (calculating UniCafe statistics and displaying results)

--- part-1/unicafe/src/App.jsx
+++ part-1/unicafe/src/App.jsx
@@ -18,21 +18,62 @@ const App = () => {
 
   // Sets total number of clicks for any given feedback button.
   const handleClick = (name) => {
-    switch (name) {
-      case 'Good':
-        setGoodClicks(goodClicks + 1);
-        break;
-      case 'Neutral':
-        setNeutralClicks(neutralClicks + 1);
-        break;
-      case 'Bad':
-        setBadClicks(badClicks + 1);
-        break;
-      default:
-        console.log(`Something went wrong while clicking the ${name} button.`);
-        break;
+   switch (name) {
+     case 'Good':
+       setGoodClicks(goodClicks + 1);
+       //   setTotalClicks(calculateTotalClicks());
+       break;
+     case 'Neutral':
+       setNeutralClicks(neutralClicks + 1);
+       break;
+     case 'Bad':
+       setBadClicks(badClicks + 1);
+       break;
+     default:
+       console.log(`Something went wrong while clicking the ${name} button.`);
+       break;
+   }
+ };
+
+  const calculateTotalClicks = () => goodClicks + neutralClicks + badClicks;
+
+  const calculateAverageRating = () => {
+    let averageRating;
+    const goodRating = 1;
+    const neutralRating = 0;
+    const badRating = -1;
+
+    const goodScore = goodRating * goodClicks;
+    const neutralScore = neutralRating * neutralClicks;
+    const badScore = badRating * badClicks;
+
+    const scoreSum = goodScore + neutralScore + badScore;
+
+    if (totalClicks === 0) {
+      averageRating =
+        'The restaurant does not yet have a rating, since no clicks have been made.';
+    } else {
+      averageRating = scoreSum / totalClicks;
     }
+
+    return averageRating;
   };
+
+  const calculatePositiveFeedbackPercentage = () => {
+    let feedbackPercentage;
+
+    if (totalClicks === 0) {
+      feedbackPercentage = 'The restaurant has not received any feedback yet.';
+    } else {
+      feedbackPercentage = (goodClicks / totalClicks * 100) + '%';
+    }
+
+    return feedbackPercentage;
+  };
+
+  const totalClicks = calculateTotalClicks();
+  const unicafeAverageRating = calculateAverageRating();
+  const positiveFeedbackPercentage = calculatePositiveFeedbackPercentage();
 
   return (
     <>
@@ -47,6 +88,9 @@ const App = () => {
         <div>Number of Good Clicks: {goodClicks}</div>
         <div>Number of Neutral Clicks: {neutralClicks}</div>
         <div>Number of Bad Clicks: {badClicks}</div>
+        <div>Total Clicks: {totalClicks}</div>
+        <div>Average Restaurant Rating Score: {unicafeAverageRating}</div>
+        <div>Positive Feedback Percentage: {positiveFeedbackPercentage}</div>
       </div>
     </>
   );
