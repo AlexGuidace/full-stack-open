1e22d9b911ebff2518b16f0cdfc5b81bdec29911
Alex Guidace <alexguidace@gmail.com>
2024-04-16 13:51:37 -0700

    Complete courseinfo_part-2 app. Complete courseinfo_part-2 Exercises 2.4 and 2.5 (extend application to allow for an arbitrary number of courses. Make Course component a separate module (this was completed in prior Exercise 2.1 commit)

--- part-2/courseinfo_part-2/src/components/TotalExercises.jsx
+++ part-2/courseinfo_part-2/src/components/TotalExercises.jsx
@@ -2,7 +2,7 @@
 const TotalExercises = ({ parts }) => {
   const total = parts.reduce((results, part) => results + part.exercises, 0);
 
-  return <p>The course has a total of {total} exercises.</p>;
+  return <p>... This course has a total of {total} exercises.</p>;
 };
 
 export default TotalExercises;
