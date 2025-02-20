891bf0410ae1583c52d0ece987de7bc6d5fc9f55
Alex Guidace <alexguidace@gmail.com>
2025-02-19 17:20:24 -0800

    Complete Exercise 4.2 (Separate index.js into two modules: index.js and app.js. Organize import order in app.js. Finalize app refactor-into-modules and verify app is still stable through browser testing)

--- part-4/blog-list-app/backend/index.js
+++ part-4/blog-list-app/backend/index.js
@@ -1,37 +1,10 @@
+// index.js is the access/starting point of application.
+
+const app = require('./app');
 const config = require('./utils/config');
-const express = require('express');
-const app = express();
-const cors = require('cors');
-const blogsRouter = require('./controllers/blogs');
-const middleware = require('./utils/middleware');
 const logger = require('./utils/logger');
-const mongoose = require('mongoose');
 
-mongoose.set('strictQuery', false);
-
-logger.info(`Connecting to database: ${config.MONGODB_URI}`);
-
-mongoose
-  .connect(config.MONGODB_URI)
-  .then(() => {
-    logger.info('Connected to database');
-  })
-  .catch((error) => {
-    logger.error('Error connecting to database: ', error.message);
-  });
-
-app.use(cors());
-app.use(express.static('dist'));
-app.use(express.json());
-app.use(middleware.requestLogger);
-
-app.use('/api/blogs', blogsRouter);
-
-app.use(middleware.unknownEndpoint);
-app.use(middleware.errorHandler);
-
-const PORT = process.env.PORT;
 // Runs locally on http://127.0.0.1:PORT/
-app.listen(PORT, () => {
-  logger.info(`Server running on port ${PORT}`);
+app.listen(config.PORT, () => {
+  logger.info(`Server running on port ${config.PORT}`);
 });
