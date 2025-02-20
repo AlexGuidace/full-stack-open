766128067ade271c174f109b6c095802205fc162
Alex Guidace <alexguidace@gmail.com>
2025-02-19 16:59:55 -0800

    Implement custom middleware and verify app is still stable. Test middleware from browser and Postman

--- part-4/blog-list-app/backend/controllers/blogs.js
+++ part-4/blog-list-app/backend/controllers/blogs.js
@@ -2,31 +2,35 @@ const express = require('express');
 const blogsRouter = express.Router();
 const Blog = require('../models/blog');
 
-// All of our blogs routes/endpoints.
+// All of our Blog routes/endpoints.
 
-blogsRouter.get('/', (request, response) => {
-  Blog.find({}).then((blogs) => {
-    response.json(blogs);
-  });
+blogsRouter.get('/', (request, response, next) => {
+  Blog.find({})
+    .then((blogs) => {
+      response.json(blogs);
+    })
+    .catch((error) => next(error));
 });
 
 // Check to see if a blog exists, based on URL.
-blogsRouter.get('/:encodedUrl', (request, response) => {
+blogsRouter.get('/:encodedUrl', (request, response, next) => {
   // Decode the URL from the previous encoding on the frontend. We need to decode it for checking here because the raw URL is what is saved in the DB when we initially POST it to the DB.
   const { encodedUrl } = request.params;
   const decodedUrl = decodeURIComponent(encodedUrl);
 
-  Blog.exists({ url: decodedUrl }).then((result) => {
-    if (result) {
-      const id = String(result._id);
-      response.json(id);
-    } else {
-      response.json(null);
-    }
-  });
+  Blog.exists({ url: decodedUrl })
+    .then((result) => {
+      if (result) {
+        const id = String(result._id);
+        response.json(id);
+      } else {
+        response.json(null);
+      }
+    })
+    .catch((error) => next(error));
 });
 
-blogsRouter.post('/', (request, response) => {
+blogsRouter.post('/', (request, response, next) => {
   const body = request.body;
 
   // Assign body a field of 'likes' and give body.likes a numerical value based on whether the "Do you love this blog?" checkbox was checked or not.
@@ -44,20 +48,23 @@ blogsRouter.post('/', (request, response) => {
     likes: body.likes,
   });
 
-  blog.save().then((result) => {
-    response.status(201).json(result);
-  });
+  blog
+    .save()
+    .then((result) => {
+      response.status(201).json(result);
+    })
+    .catch((error) => next(error));
 });
 
-blogsRouter.put('/:id', (request, response) => {
+blogsRouter.put('/:id', (request, response, next) => {
   const { id } = request.params;
 
   // Use MongoDB $inc operator to increment the value of the likes field by 1 because the checkbox was checked on the frontend for this submission.
-  Blog.findByIdAndUpdate(id, { $inc: { likes: 1 } }, { new: true }).then(
-    (result) => {
+  Blog.findByIdAndUpdate(id, { $inc: { likes: 1 } }, { new: true })
+    .then((result) => {
       response.json(result);
-    }
-  );
+    })
+    .catch((error) => next(error));
 });
 
 module.exports = blogsRouter;
--- part-4/blog-list-app/backend/utils/middleware.js
+++ part-4/blog-list-app/backend/utils/middleware.js
@@ -2,12 +2,14 @@
 
 const logger = require('./logger');
 
-const requestLogger = (request) => {
+const requestLogger = (request, response, next) => {
   logger.info('HTTP REQUEST INFO: ');
   logger.info('Method: ', request.method);
   logger.info('Path: ', request.path);
   logger.info('Body: ', request.body);
   logger.info('-------------------------------');
+
+  next();
 };
 
 const unknownEndpoint = (request, response) => {
