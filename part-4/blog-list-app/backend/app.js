afcecb3ee6e6a8ce5fd86cce0407c7bab4638f2a
Alex Guidace <alexguidace@gmail.com>
2025-08-11 16:44:42 -0700

    Begin Exercises 4.15 - 4.23: --- Complete 4.15: Blog List Expansion, Step 3 --- - Implement User model and POST request to address api/users - Implement bcrypt library for password hashing - Implement GET request to display all users client-side - Verify functionalities through Postman and REST Client

--- part-4/blog-list-app/backend/app.js
+++ part-4/blog-list-app/backend/app.js
@@ -11,6 +11,7 @@ const mongoose = require('mongoose');
 const logger = require('./utils/logger');
 const middleware = require('./utils/middleware');
 const blogsRouter = require('./controllers/blogs');
+const usersRouter = require('./controllers/users');
 
 mongoose.set('strictQuery', false);
 
@@ -31,6 +32,7 @@ app.use(express.json());
 app.use(middleware.requestLogger);
 
 app.use('/api/blogs', blogsRouter);
+app.use('/api/users', usersRouter);
 
 app.use(middleware.unknownEndpoint);
 app.use(middleware.errorHandler);
