42ebfc9e9beeee3e94fae6b268e13d980990c8fc
Alex Guidace <alexguidace@gmail.com>
2025-10-13 06:37:21 -0700

    --- Complete 5.14: Step 2 --- - Create a test that checks that a Blog component's URL and number of likes are shown when the 'View Details' button is clicked

--- part-4/blog-list-app/frontend/src/components/Blog.test.jsx
+++ part-4/blog-list-app/frontend/src/components/Blog.test.jsx
@@ -1,4 +1,5 @@
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import Blog from './Blog';
 
 test('Checks that a Blog component displays its blog title and author on render. Checks that a Blog component does not display its URL or number of Likes by default.', () => {
@@ -9,7 +10,6 @@ test('Checks that a Blog component displays its blog title and author on render.
     likes: 23,
     user: { username: 'Test User' },
   };
-
   // Provide a username so the Delete button inside the blog doesn't crash.
   const user = { username: 'Test User' };
 
@@ -23,4 +23,26 @@ test('Checks that a Blog component displays its blog title and author on render.
 
   expect(url).not.toBeVisible();
   expect(likes).not.toBeVisible();
+});
+
+test('Checks that a Blog component`s URL and number of likes are shown when the `View Details` button is clicked.', async () => {
+  const blog = {
+    title: 'Test Blog',
+    author: 'Test Author',
+    url: 'www.test',
+    likes: 23,
+    user: { username: 'Test User' },
+  };
+  // Provide a username so the Delete button inside the blog doesn't crash.
+  const userObject = { username: 'Test User' };
+
+  const user = userEvent.setup();
+
+  render(<Blog blog={blog} user={userObject} />);
+
+  const viewDetailsButton = screen.getByText('View Details');
+  await user.click(viewDetailsButton);
+
+  expect(screen.getByText(blog.likes)).toBeDefined();
+  expect(screen.getByText(blog.url)).toBeDefined();
 });
